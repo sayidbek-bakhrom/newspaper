4f4792d5e215426000e7978f97cc46cf2435ff57
user <sayidbek-bakhrom>
2022-03-16 16:31:13 +0500

    Initial commit

---
+++ articles/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from . import views
+
+
+urlpatterns = [
+    path('', views.ArticleListView.as_view(), name='article_list'),
+    path('<int:pk>/edit/', views.ArticleUpdateView.as_view(), name='article_edit'),
+    path('<int:pk>/delete/', views.ArticleDeleteView.as_view(), name='article_delete'),
+    path('<int:pk>/', views.ArticleDetailView.as_view(), name='article_detail'),
+    path('new/', views.ArticleCreateView.as_view(), name='article_new'),
+
+]
